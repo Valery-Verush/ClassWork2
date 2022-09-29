e7e5614c2dcc6b51eec5cbe5b412320c3fd26313
Valery_Verush <valerush8@gmail.com>
2022-09-29 22:37:50 +0300

    homeWork 3

--- src/index.js
+++ src/index.js
@@ -1,72 +1,16 @@
 import "./main.scss";
 
-let userSecondName;
-while (true) {
-  userSecondName = prompt("Введите вашу фамилию");
-  if (typeof userSecondName == "string" && userSecondName != "") {
-    break;
-  } else {
-    alert("Не указана фамилия пользователя");
+let arrayNumb = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
+
+const TREESUM = (arrayNumb, sumArray = 0) => {
+  for (let i in arrayNumb) {
+    if (typeof arrayNumb[i] == "number") {
+      sumArray += arrayNumb[i];
+    } else {
+      sumArray += TREESUM(arrayNumb[i]);
+    }
   }
-}
+  return sumArray;
+};
 
-let userFirstName;
-while (true) {
-  userFirstName = prompt("Введите ваше имя");
-  if (typeof userFirstName == "string" && userFirstName != "") {
-    break;
-  } else {
-    alert("Не указано имя пользователя");
-  }
-}
-
-let userFatherName;
-while (true) {
-  userFatherName = prompt("Введите ваше отчество");
-  if (typeof userFatherName == "string" && userFatherName != "") {
-    break;
-  } else {
-    alert("Не указано отчество пользователя");
-  }
-}
-
-let userAge;
-while (true) {
-  userAge = prompt("Введите ваш возраст");
-  userAge = +userAge;
-  if (typeof userAge == "number" && userAge > 0) {
-    break;
-  } else {
-    alert("Не указан возраст пользователя");
-  }
-}
-
-let isUserMenGender;
-let UserGender;
-isUserMenGender = confirm("Ваш пол мужской ?");
-if (isUserMenGender == true) {
-  UserGender = "Мужской";
-} else {
-  UserGender = "Женский";
-}
-
-let isUserOnPension;
-if (
-  (isUserMenGender == true && userAge < 62) ||
-  (isUserMenGender == false && userAge < 57)
-) {
-  isUserOnPension = "нет";
-} else {
-  isUserOnPension = "да";
-}
-
-const questionnaire = `ваше ФИО: ${
-  userSecondName + " " + userFirstName + " " + userFatherName
-}
-ваш возраст в годах: ${userAge}
-ваш возраст в днях: ${userAge * 365}
-через 5 лет вам будет: ${userAge + 5}
-ваш пол: ${UserGender}
-вы на пенсии: ${isUserOnPension}`;
-
-alert(questionnaire);
+console.log(TREESUM(arrayNumb));
