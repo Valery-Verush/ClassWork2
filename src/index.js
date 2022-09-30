d8d3dfff0f6c1786c4f80c5dc59a5ab11dd2833f
Valery_Verush <valerush8@gmail.com>
2022-09-30 13:04:41 +0300

    homeWork 4.1

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import "./main.scss";
 
 let arrayNumb = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
 
-const TREESUM = (arrayNumb, sumArray = 0) => {
+const treeSum = (arrayNumb, sumArray = 0) => {
   for (let i in arrayNumb) {
     if (typeof arrayNumb[i] == "number") {
       sumArray += arrayNumb[i];
     } else {
-      sumArray += TREESUM(arrayNumb[i]);
+      sumArray += treeSum(arrayNumb[i]);
     }
   }
   return sumArray;
 };
 
-console.log(TREESUM(arrayNumb));
+console.log(treeSum(arrayNumb));
